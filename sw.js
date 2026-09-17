5285f824d0f4b77b1204094f5db922e770d9d30c
Nishant <nshntdhkr@gmail.com>
2026-09-17 12:51:30 +0545

    chore: bump version to 5.6.21

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js
-const CACHE_NAME = "keepmoviez-local-v5.6.20";
+const CACHE_NAME = "keepmoviez-local-v5.6.21";
 const OFFLINE_URL = "offline.html";
 const SUPABASE_URL = "https://ujnjtvlkxhdbdbngdaeb.supabase.co";
 
@@ -36,18 +36,18 @@ const CORE_ASSETS = [
   // App Logic Scripts
   "./js/constant.js?v=5.6.20",
   "./js/backfill.js?v=5.6.20",
-  "./js/utils.js?v=5.6.20",
+  "./js/utils.js?v=5.6.21",
   "./js/indexeddb.js?v=5.6.20",
   "./js/data.js?v=5.6.20",
   "./js/input-output.js?v=5.6.20",
   "./js/tmdb.js?v=5.6.20",
   "./js/genre.js?v=5.6.20",
   "./js/analysis.js?v=5.6.20",
-  "./js/ui.js?v=5.6.20",
-  "./js/reporting.js?v=5.6.20",
-  "./js/app.js?v=5.6.20",
-  "./js/supabase.js?v=5.6.20",
-  "./js/main.js?v=5.6.20",
+  "./js/ui.js?v=5.6.21",
+  "./js/reporting.js?v=5.6.21",
+  "./js/app.js?v=5.6.21",
+  "./js/supabase.js?v=5.6.21",
+  "./js/main.js?v=5.6.21",
 
   // Icons
   "./icons/icon-192x192.png",
